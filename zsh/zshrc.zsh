3b58973129e6eac92d19705f0e1f290eb3a3b06b
Luo Boming <luo.boming@fixstars.com>
2021-12-03 17:58:58 +0900

    Add options to grep not to mess up message

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -42,7 +42,7 @@ autoload -U colors && colors
 
 export LS_COLORS=di="1;34:ln=35:so=32:pi=33:ex=31:bd=30;46:cd=30;43:su=30;41:sg=30;46:tw=37;42:ow=30;43"
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-alias grep='grep --color=auto'
+alias grep='grep --color=auto --exclude-dir=".git" --exclude-dir=".ccls-cache"'
 
 case $OSTYPE in
 linux*)
