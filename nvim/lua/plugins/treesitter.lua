eed0d4562172fbe5c42d80fce58e31c671f361c0
Luo Boming <luo.boming@fixstars.com>
2022-09-28 18:07:48 +0900

    add rust to treesitter support

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-   ensure_installed = {'bash', 'c', 'cpp', 'lua', 'python'},
+   ensure_installed = {'bash', 'c', 'cpp', 'lua', 'python', 'rust'},
    sync_install = false,
    auto_install = true,
    highlight = {enable = true, additional_vim_regex_highlighting = false}
