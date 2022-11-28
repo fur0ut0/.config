6f73f69b8e0c0803a2f48af038606aea937d4310
Boming Luo <boming.luo@woven-planet.global>
2022-11-28 14:25:54 +0900

    fix deprecated function issue

--- nvim/lua/plugins/mason_lspconfig.lua
+++ nvim/lua/plugins/mason_lspconfig.lua
@@ -1,7 +1,7 @@
 require('mason-lspconfig').setup_handlers({
    function(server)
       local opt = {
-         capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+         capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
       }
       require('lspconfig')[server].setup(opt)
    end
