31efbcc25fd93d169a0c4889c1596ef43e323bb4
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-03-06 12:39:53 +0900

    Add fzf installation script

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ module_dir="$this_dir"/.config_entities/script/module
 . "$module_dir"/find_out_distro.sh
 . "$module_dir"/confirm_install.sh
 . "$module_dir"/install_fish.sh
-. "$module_dir"/install_pyenv.sh
+. "$module_dir"/install_fzf.sh
 . "$module_dir"/install_neovim.sh
+. "$module_dir"/install_pyenv.sh
 
 confirm_install fish
-confirm_install pyenv
+confirm_install fzf
 confirm_install neovim
+confirm_install pyenv
