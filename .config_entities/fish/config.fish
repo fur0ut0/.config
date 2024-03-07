2cfd5190e044ce8487688edc8a5d5a7aaa795351
LUO Boming <luo.boming@fixstars.com>
2024-03-07 15:22:39 +0900

    Start tmux in VSCode terminal

--- .config_entities/fish/config.fish
+++ .config_entities/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     alias vim="vim -u $XDG_CONFIG_HOME/vim/vimrc"
 
     # Commands to run in interactive sessions can go here
-    if status is-login; and not set -q TMUX; and [ "$TERM_PROGRAM" != vscode ]
+    if status is-login; and not set -q TMUX
         attach-tmux
     end
 end
