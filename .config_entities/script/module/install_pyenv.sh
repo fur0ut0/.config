f9ce6885f603430c0adebbf666af786d7d14a344
Boming LUO <e-fix-luo@toyota-tokyo.tech>
2024-03-08 11:21:00 +0900

    Add -y to apt install

--- .config_entities/script/module/install_pyenv.sh
+++ .config_entities/script/module/install_pyenv.sh
@@ -21,7 +21,7 @@ install_pyenv() {
    # FIY: https://github.com/pyenv/pyenv/wiki#troubleshooting--faq
    if which apt > /dev/null; then
       sudo apt update
-      sudo apt install build-essential libssl-dev zlib1g-dev libbz2-dev \
+      sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
          libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev \
          libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
    else
