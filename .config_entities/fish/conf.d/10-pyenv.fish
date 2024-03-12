c74bbb5dfe8c2d81f5adb2c916a86a592aacbf9b
Boming LUO <fulhelm.gadgetter@gmail.com>
2024-03-12 12:01:52 +0900

    Use fish_add_path to config path

--- .config_entities/fish/conf.d/10-pyenv.fish
+++ .config_entities/fish/conf.d/10-pyenv.fish
@@ -1,5 +1,5 @@
 set -q PYENV_ROOT; or set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv
 if [ -d $PYENV_ROOT/bin ]
-    set -x -a PATH $PYENV_ROOT/bin
+    fish_add_path $PYENV_ROOT/bin
     eval "$(pyenv init -)"
 end
