249955b0d1008231b846705465d21e40fc7688fa
Boming LUO <fulhelm.gadgetter@gmail.com>
2024-03-13 17:13:08 +0900

    Prevent tmux attach when there is no tmux

--- .config_entities/fish/config.fish
+++ .config_entities/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     alias vim="vim -u $XDG_CONFIG_HOME/vim/vimrc"
 
     # Commands to run in interactive sessions can go here
-    if status is-login; and not set -q TMUX
+    if status is-login; and not set -q TMUX; and which tmux > /dev/null
         attach-tmux
     end
 end
