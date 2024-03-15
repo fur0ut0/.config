3a6a4e706b5dec4c62c4c3343506c720870eb0f6
Boming LUO <e-fix-at@toyota-tokyo.tech>
2024-03-15 21:51:42 +0900

    Use alias for old version tmux

--- .config_entities/fish/config.fish
+++ .config_entities/fish/config.fish
@@ -1,5 +1,11 @@
 if status is-interactive
     alias vim="vim -u $XDG_CONFIG_HOME/vim/vimrc"
+    if which tmux > /dev/null;
+        set -l ver (tmux -V | grep -o -E "[0-9]+\.[0-9]+")
+        if [ (echo $ver'<'3.1 | bc -l) -eq 1 ]
+            alias tmux="tmux -f $TMUX_CONFIG_DIR/tmux.conf"
+        end
+    end
 
     # Commands to run in interactive sessions can go here
     if status is-login; and not set -q TMUX; and which tmux > /dev/null
