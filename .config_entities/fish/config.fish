817e7b183c4a4f0cbdf6c6feffd7d587b75eedcd
Boming LUO <fulhelm.gadgetter@gmail.com>
2024-03-15 09:34:35 +0900

    Propagate updated DISPLAY variable on login

--- .config_entities/fish/config.fish
+++ .config_entities/fish/config.fish
@@ -3,6 +3,8 @@ if status is-interactive
 
     # Commands to run in interactive sessions can go here
     if status is-login; and not set -q TMUX; and which tmux > /dev/null
+        # propagate DISPLAY info to existing shells
+        set -U -x DISPLAY $DISPLAY
         attach-tmux
     end
 end
